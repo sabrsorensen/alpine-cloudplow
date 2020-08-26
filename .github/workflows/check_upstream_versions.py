aba301afc3e1513b7a77b310b1bd0740653ba9f0
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2020-08-26 00:01:11 -0600

    Updated s6-overlay.

--- .github/workflows/check_upstream_versions.py
+++ .github/workflows/check_upstream_versions.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 ###############################################################################
-# Query for the latest versions of rclone and cloudplow
+# Query for the latest versions of rclone, cloudplow, and s6-overlay
 ###############################################################################
 import docker
 import json
@@ -21,6 +21,8 @@ try:
         os.environ['GITHUB_REPOSITORY'], command='git --work-tree=/opt/cloudplow rev-parse HEAD', auto_remove=True, entrypoint='').decode('UTF-8').strip()
     old_versions['alpine-cloudplow_commit_ref'] = client.images.get(
         os.environ['GITHUB_REPOSITORY']).labels['org.label-schema.vcs-ref']
+    old_versions['s6_release'] = client.containers.run(
+        os.environ['GITHUB_REPOSITORY'], command='touch /etc/S6_RELEASE; cat /etc/S6_RELEASE', auto_remove=True, entrypoint='').decode('UTF-8').strip()
     print("Detected image versions:\n" + json.dumps(old_versions, indent=2))
 except docker.errors.ContainerError as e:
     print(e)
@@ -31,6 +33,8 @@ current_versions['cloudplow_commit_ref'] = requests.get(
     'https://api.github.com/repos/l3uddz/cloudplow/commits/develop').json()["sha"]
 current_versions['alpine-cloudplow_commit_ref'] = Repo(
     '.').commit('HEAD').hexsha
+current_versions['s6_release'] = requests.get(
+    'https://api.github.com/repos/just-containers/s6-overlay/releases/latest').json()["tag_name"]
 
 print("Current detected versions:\n" + json.dumps(current_versions, indent=2))
 
--- Dockerfile
+++ Dockerfile
@@ -41,8 +41,11 @@ RUN apk -U add --no-cache \
     python3 -m pip install --no-cache-dir --upgrade pip
 
 # install s6-overlay for process management
-ADD https://github.com/just-containers/s6-overlay/releases/download/v1.22.1.0/s6-overlay-amd64.tar.gz /tmp/
-RUN tar xzf /tmp/s6-overlay-amd64.tar.gz -C /
+RUN curl -sX GET "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]' > /etc/S6_RELEASE && \
+    wget https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-amd64.tar.gz -O /tmp/s6-overlay-amd64.tar.gz && \
+    tar xzf /tmp/s6-overlay-amd64.tar.gz -C / && \
+    rm /tmp/s6-overlay-amd64.tar.gz && \
+    echo "Installed s6-overlay `cat /etc/S6_RELEASE`"
 
 # download cloudplow
 RUN git clone --depth 1 --single-branch --branch develop https://github.com/l3uddz/cloudplow /opt/cloudplow
