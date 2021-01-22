dda27185ac4c5969c3579a7288fe9ba37cb43065
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-01-22 13:34:20 -0700

    Fixed devtainer.

--- root/tmp/get_s6-overlay.sh
+++ root/tmp/get_s6-overlay.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-case ${ARCH} in
+if [ -n $ARCH ]
+then
+  s6_arch=$ARCH
+else
+  s6_arch="amd64"
+fi
+
+case "$s6_arch" in
   arm-v7)
     ARCH="armhf"
     ;;
@@ -11,10 +18,10 @@ case ${ARCH} in
     ARCH="amd64"
     ;;
 esac
-echo $ARCH
 curl -sX GET "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]' >/etc/S6_RELEASE && \
-echo "Dowloading from https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-${ARCH}.tar.gz" && \
-wget https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-${ARCH}.tar.gz -O /tmp/s6-overlay.tar.gz && \
+s6_url="https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-$s6_arch.tar.gz"
+echo "Downloading from $s6_url" && \
+wget $s6_url -O /tmp/s6-overlay.tar.gz && \
 tar xzf /tmp/s6-overlay.tar.gz -C / && \
 rm /tmp/s6-overlay.tar.gz && \
-echo "Installed s6-overlay `cat /etc/S6_RELEASE` ($ARCH)"
+echo "Installed s6-overlay `cat /etc/S6_RELEASE` ($s6_arch)"
