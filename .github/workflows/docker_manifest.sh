06c3388dffd699b95b8833aa52f709d2a94f1522
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-02-27 18:09:30 -0700

    Cherry-picked workflow improvements from init_config.

--- .github/workflows/docker_manifest.sh
+++ .github/workflows/docker_manifest.sh
@@ -5,8 +5,30 @@ docker manifest create ${GITHUB_REPOSITORY}:${GITHUB_SHA:0:7} \
     --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
     --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
 docker manifest push ${GITHUB_REPOSITORY}:${GITHUB_SHA:0:7}
-docker manifest create ${GITHUB_REPOSITORY}:latest \
-    --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
-    --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
-    --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
-docker manifest push ${GITHUB_REPOSITORY}:latest
+
+if [[ ${GITHUB_REF//refs\/heads\//} == "main" ]]
+then
+    docker manifest create ${GITHUB_REPOSITORY}:latest \
+        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
+        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
+        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
+    docker manifest push ${GITHUB_REPOSITORY}:latest
+
+    docker manifest create ${GITHUB_REPOSITORY}:amd64 \
+        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64
+    docker manifest push ${GITHUB_REPOSITORY}:amd64
+
+    docker manifest create ${GITHUB_REPOSITORY}:arm-v7 \
+        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7
+    docker manifest push ${GITHUB_REPOSITORY}:arm-v7
+
+    docker manifest create ${GITHUB_REPOSITORY}:arm64 \
+        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
+    docker manifest push ${GITHUB_REPOSITORY}:arm64
+else
+    docker manifest create ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//} \
+        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
+        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
+        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
+    docker manifest push ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}
+fi
