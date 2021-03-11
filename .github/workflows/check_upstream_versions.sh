3123ff43a1ea259ce256d9dfc8b271748b3db368
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-03-10 23:27:13 -0700

    Changed rclone version check to reference docker image.

--- .github/workflows/check_upstream_versions.sh
+++ .github/workflows/check_upstream_versions.sh
@@ -2,11 +2,13 @@
 
 image_cloudplow_ref=`docker run $GITHUB_REPOSITORY "git --work-tree=/opt/cloudplow rev-parse HEAD"`
 image_s6_release=`docker run $GITHUB_REPOSITORY "cat /etc/S6_RELEASE"`
+image_rclone_version=`docker run $GITHUB_REPOSITORY rclone version`
 
 current_cloudplow_ref=`curl -sX GET "https://api.github.com/repos/l3uddz/cloudplow/commits/develop" | jq '.sha' | tr -d '"'`
 current_s6_release=`curl -sX GET "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | jq '.tag_name' | tr -d '"'`
+current_rclone_version=`docker run rclone/rclone version`
 
-if [ `docker run $GITHUB_REPOSITORY "rclone version --check" | grep -P "yours|latest" | awk '{print $2}' | uniq | wc -l` == 2 ]
+if [ $image_rclone_version != $current_rclone_version ]
 then
     echo "New version of rclone is available."
     build=1
