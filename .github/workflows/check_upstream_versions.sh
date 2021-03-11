50b4afc5983599b4b0747b1236b2bab34876d099
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-03-10 23:30:54 -0700

    Fixed version check command.

--- .github/workflows/check_upstream_versions.sh
+++ .github/workflows/check_upstream_versions.sh
@@ -2,11 +2,11 @@
 
 image_cloudplow_ref=`docker run $GITHUB_REPOSITORY "git --work-tree=/opt/cloudplow rev-parse HEAD"`
 image_s6_release=`docker run $GITHUB_REPOSITORY "cat /etc/S6_RELEASE"`
-image_rclone_version=`docker run $GITHUB_REPOSITORY rclone version`
+image_rclone_version=`docker run $GITHUB_REPOSITORY "rclone version"`
 
 current_cloudplow_ref=`curl -sX GET "https://api.github.com/repos/l3uddz/cloudplow/commits/develop" | jq '.sha' | tr -d '"'`
 current_s6_release=`curl -sX GET "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | jq '.tag_name' | tr -d '"'`
-current_rclone_version=`docker run rclone/rclone version`
+current_rclone_version=`docker run rclone/rclone "version"`
 
 if [ $image_rclone_version != $current_rclone_version ]
 then
