44b86de70b488e4b19663f8a7587c284ebb6b54c
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-06-25 17:13:25 -0600

    Fixed workflows for PR and branch work. (#12)

--- .github/workflows/docker_build_pr.sh
+++ .github/workflows/docker_build_pr.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+TAG=${GITHUB_REF//refs\/pull\//PR}
+TAG=${TAG//\/merge/}
 docker buildx build \
     --platform ${ARCHITECTURE//-/\/} \
     --output "type=image,push=false" \
-    --tag ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-${ARCHITECTURE//linux-/} \
+    --tag ${GITHUB_REPOSITORY}:${TAG}-${GITHUB_SHA:0:7}-${ARCHITECTURE//linux-/} \
     --build-arg ARCH="${ARCHITECTURE//linux-/}" \
     --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
     --build-arg COMMIT_AUTHOR="$(git log -1 --pretty=format:'%ae')" \
