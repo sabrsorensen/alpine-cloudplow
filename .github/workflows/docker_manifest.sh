04c0ca8417aa991b6c4d41c22da6cc2a33f7358a
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-08-29 20:09:51 -0600

    Fixed publish.

--- .github/workflows/docker_manifest.sh
+++ .github/workflows/docker_manifest.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-docker manifest create ${GITHUB_REPOSITORY}:${GITHUB_SHA:0:7} \
-    --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
-    --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
-    --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
-docker manifest push ${GITHUB_REPOSITORY}:${GITHUB_SHA:0:7}
+docker manifest create ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_SHA:0:7} \
+    --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
+    --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
+    --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
+docker manifest push ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_SHA:0:7}
 
 if [[ ${GITHUB_REF//refs\/heads\//} == "main" ]]
 then
-    docker manifest create ${GITHUB_REPOSITORY}:latest \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
-    docker manifest push ${GITHUB_REPOSITORY}:latest
+    docker manifest create ghcr.io/${GITHUB_REPOSITORY}:latest \
+        --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
+        --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
+        --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
+    docker manifest push ghcr.io/${GITHUB_REPOSITORY}:latest
 
-    docker manifest create ${GITHUB_REPOSITORY}:amd64 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64
-    docker manifest push ${GITHUB_REPOSITORY}:amd64
+    docker manifest create ghcr.io/${GITHUB_REPOSITORY}:amd64 \
+        --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64
+    docker manifest push ghcr.io/${GITHUB_REPOSITORY}:amd64
 
-    docker manifest create ${GITHUB_REPOSITORY}:arm-v7 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7
-    docker manifest push ${GITHUB_REPOSITORY}:arm-v7
+    docker manifest create ghcr.io/${GITHUB_REPOSITORY}:arm-v7 \
+        --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7
+    docker manifest push ghcr.io/${GITHUB_REPOSITORY}:arm-v7
 
-    docker manifest create ${GITHUB_REPOSITORY}:arm64 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
-    docker manifest push ${GITHUB_REPOSITORY}:arm64
+    docker manifest create ghcr.io/${GITHUB_REPOSITORY}:arm64 \
+        --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
+    docker manifest push ghcr.io/${GITHUB_REPOSITORY}:arm64
 else
-    docker manifest create ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//} \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
-    docker manifest push ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}
+    docker manifest create ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//} \
+        --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
+        --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
+        --amend ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
+    docker manifest push ghcr.io/${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}
 fi
