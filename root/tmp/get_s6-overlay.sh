ddc62f8bcd07c797a8c429e0d6ffd2be86777f3c
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2022-03-11 23:52:50 -0700

    fixed s6-overlay urls.

--- root/tmp/get_s6-overlay.sh
+++ root/tmp/get_s6-overlay.sh
@@ -21,22 +21,26 @@ esac
 curl -sX GET "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]' >/etc/S6_RELEASE && \
 s6_arch_url="https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-$s6_arch.tar.xz"
 s6_noarch_url="https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-noarch.tar.xz"
-s6_symlinks_arch_url="https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-$s6_arch.tar.xz"
-s6_symlinks_noarch_url="https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-noarch.tar.xz"
+s6_symlinks_arch_url="https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-symlinks-$s6_arch.tar.xz"
+s6_symlinks_noarch_url="https://github.com/just-containers/s6-overlay/releases/download/`cat /etc/S6_RELEASE`/s6-overlay-symlinks-noarch.tar.xz"
+
 echo "Downloading from $s6_noarch_url" && \
-wget $s6_arch_url -O /tmp/s6-overlay-noarch.tar.xz && \
-tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.xz -C / && \
+wget $s6_noarch_url -O /tmp/s6-overlay-noarch.tar.xz && \
+tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.xz && \
 rm /tmp/s6-overlay-noarch.tar.xz && \
+
 echo "Downloading from $s6_arch_url" && \
 wget $s6_arch_url -O /tmp/s6-overlay-${s6_arch}.tar.xz && \
 tar -C / -Jxpf /tmp/s6-overlay-${s6_arch}.tar.xz && \
 rm /tmp/s6-overlay-${s6_arch}.tar.xz && \
+
 echo "Downloading from $s6_symlinks_noarch_url" && \
-wget $s6_arch_url -O /tmp/s6-overlay-symlinks-noarch.tar.xz && \
+wget $s6_symlinks_noarch_url -O /tmp/s6-overlay-symlinks-noarch.tar.xz && \
 tar -C / -Jxpf /tmp/s6-overlay-symlinks-noarch.tar.xz && \
 rm /tmp/s6-overlay-symlinks-noarch.tar.xz && \
+
 echo "Downloading from $s6_symlinks_arch_url" && \
-wget $s6_arch_url -O /tmp/s6-overlay-symlinks-arch.tar.xz && 
+wget $s6_symlinks_arch_url -O /tmp/s6-overlay-symlinks-arch.tar.xz && 
 tar -C / -Jxpf /tmp/s6-overlay-symlinks-arch.tar.xz && \
 rm /tmp/s6-overlay-symlinks-arch.tar.xz && \
 echo "Installed s6-overlay `cat /etc/S6_RELEASE` ($s6_arch)"
